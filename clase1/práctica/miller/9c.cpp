6a4e806ec93b2b3d43f1e09578bbe94f2e5f0d07
Marcos Gabriel Miller <marcosgabrielmiller@gmail.com>
2021-04-05 14:46:40 -0300

    problemas resueltos hasta ahora

---
+++ clase1/práctica/miller/9c.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int fibo(int i);
+
+int main(){
+    int iteraciones, out;
+    cout << "Ingrese la cantidad de iteraciones a realizar\n";
+    cin >> iteraciones;
+    out = fibo(iteraciones);
+    cout << out;
+    return 0;
+}
+
+int fibo(int i){
+    int f0 = 0;
+    int f1 = 1;
+    if(i != 0)
+        return (fibo(a, n-1));
+    return 1;
+}
